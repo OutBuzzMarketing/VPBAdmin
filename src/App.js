d2b7f7574fa38ee09a2ad7289f08535bdec6187e
Surbhi <outbuzzmarketing@gmail.com>
2020-11-10 17:03:05 +0530

    Test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
-import React, { Component } from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
